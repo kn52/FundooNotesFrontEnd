25347a5520832dabf401b5f305138b4de9d44426
kn52 <ashish52922@gmail.com>
2020-09-09 14:53:04 +0530

    [Aashish]ADDED:Route for forget password

--- src/route/RouteSwitch.jsx
+++ src/route/RouteSwitch.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 import React from "react";
 import SignIn from '../component/SignIn';
+import ForgetPassword from '../component/ForgetPassword';
 
 export default function DefaultRoute () {
     return (
         <BrowserRouter>
             <Switch>
                 <Route path={['/','/login']} exact component={SignIn} />
+                <Route path='/forgetpassword' exact component={ForgetPassword} />
             </Switch>
         </BrowserRouter>
     );
