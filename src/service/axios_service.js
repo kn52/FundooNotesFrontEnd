9ce5185cb0fce7aff6d044364b7ff281edff6849
kn52 <ashish52922@gmail.com>
2020-09-10 17:29:11 +0530

    [Aashish]ADDED:Axios service get post put delete method

---
+++ src/service/axios_service.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export default function axiosService(){
+}
+
+axiosService.prototype.get = function get(URL,isHeaderReq = false ,header) {
+    return axios.get(URL, isHeaderReq && header);
+}
+
+axiosService.prototype.post = function post(URL, data, isHeaderReq = false ,header) {
+    return axios.post(URL, data, isHeaderReq && header)
+}
+
+axiosService.prototype.put = function update(URL, data, isHeaderReq = false ,header) {
+    return axios.put(URL, data, isHeaderReq && header)
+}
+
+axiosService.prototype.delete = function delte(URL, isHeaderReq = false ,header) {
+    return axios.delete(URL, isHeaderReq && header)
+}
