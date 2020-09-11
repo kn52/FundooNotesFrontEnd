530d6924304071df6f58c6bb9429ed607dcd43db
kn52 <ashish52922@gmail.com>
2020-09-11 10:50:30 +0530

    [Aashish]ADDED:Route for registration form

--- src/route/RouteSwitch.jsx
+++ src/route/RouteSwitch.jsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 import React from "react";
 import SignIn from '../component/SignIn';
+import SignUp from '../component/SignUp';
 import ForgetPassword from '../component/ForgetPassword';
+import ResetPassword from '../component/ResetPassword';
 
 export default function DefaultRoute () {
     return (
         <BrowserRouter>
             <Switch>
                 <Route path={['/','/login']} exact component={SignIn} />
+                <Route path='/register' exact component={SignUp} />
                 <Route path='/forgetpassword' exact component={ForgetPassword} />
                 <Route path='/resetpassword' component={ResetPassword} />
             </Switch>
